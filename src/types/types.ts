3410746704677f2d7654227d6d92e0fb792431a7
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2025-02-05 11:59:55 +0100

    Added borrow and return endpoints

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,7 +2,9 @@ import { ColumnType, Generated, Insertable, JSONColumnType, Selectable, Updateab
 
 export interface Database {
 		books: BookTable,
-		site: SiteTable
+		site: SiteTable,
+		person: PersonTable,
+		customer: CustomerTable
 }
 
 export interface BookTable {
@@ -29,3 +31,31 @@ export interface SiteTable {
 export type Site = Selectable<SiteTable>
 export type NewSite = Insertable<SiteTable>
 export type SiteUpdate = Updateable<SiteTable>
+
+export enum PersonType {
+	"customer",
+	"employee"
+}
+
+export interface PersonTable {
+		id: string,
+		first_name: string,
+		last_name: string,
+		address_id: number,
+		role: PersonType
+}
+
+export type Person = Selectable<PersonTable>
+export type NewPerson = Insertable<PersonTable>
+export type UpdatePerson = Updateable<PersonTable>
+
+export interface CustomerTable {
+		id: string,
+		email: string,
+		phone: string,
+		person_id: number,
+}
+
+export type Customer = Selectable<CustomerTable>
+export type NewCustomer = Insertable<CustomerTable>
+export type UpdateCustomer = Updateable<CustomerTable>
